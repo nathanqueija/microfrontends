3522c8d2fc8f8a1a3c97f4cbb870629c45138975
Nathan Queija <nathanqcam@gmail.com>
2022-09-16 18:35:38 +0200

    Add prod config to marketing FE

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
 const packageJson = require('../package.json');
 
@@ -8,7 +7,6 @@ const devConfig = {
   mode: 'development',
   devServer: { port: 8081, historyApiFallback: { index: 'index.html' } },
   plugins: [
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
     new ModuleFederationPlugin({
       name: 'marketing',
       filename: 'remoteEntry.js',
---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const { ModuleFederationPlugin } = require('webpack').container;
+const packageJson = require('../package.json');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: { './MarketingMount': './src/bootstrap' },
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
